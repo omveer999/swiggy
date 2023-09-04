ee76cdadf10c5429e642bd6386ef2fa88bddbdc2
omveer.singh <omveer.singh@rakuten.com>
2023-09-04 09:55:06 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,9 @@
+import React  from "react";
+import  ReactDOM from "react-dom/client";
+
+const h1=React.createElement("h1",{},"Heading 1 Hello every one");
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(h1);
+
