5c62de09e63082b67daaa23e18aa3f83bb55496b
omveer.singh <omveer.singh@rakuten.com>
2023-09-04 10:10:54 +0530

    functional component and react element

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React  from "react";
 import  ReactDOM from "react-dom/client";
 
-const h1=React.createElement("h1",{},"Heading 1 Hello every one");
+const heading=<h1>Hello every one......</h1>;
+const ContentName=()=>(
+    <div>
+       <h3>Heading 66666</h3>
+        <p>Lorem ipsum ,dolor sit amet consectetur adipisicing elit. Sunt placeat distinctiotatum atque quis culpa quaerat dignissimos earum.</p>
+    </div> 
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(h1);
+root.render(<ContentName></ContentName>);
 
