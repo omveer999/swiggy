3f21b114d9ca3045eab7d7f95f47d80b27760295
omveer.singh <omveer.singh@rakuten.com>
2023-09-07 13:07:25 +0530

    changes for image

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
 import React  from "react";
 import  ReactDOM from "react-dom/client";
-
-const heading=<h1>Hello every one......</h1>;
-const ContentName=()=>(
-    <div>
-       <h3>Heading 66666</h3>
-        <p>Lorem ipsum ,dolor sit amet consectetur adipisicing elit. Sunt placeat distinctiotatum atque quis culpa quaerat dignissimos earum.</p>
-    </div> 
+import myImage from "./assets/img/logo.png"
+const Header=()=>(
+        <div className="navbar">
+            <div className="logo">
+                <img src={myImage} alt="logo" />
+            </div>
+            <div className="menu">
+                <ul className="menu-list">
+                    <li className="menu-item">Home</li>
+                    <li className="menu-item">About</li>
+                    <li className="menu-item">Contact Us</li>
+                </ul>
+            </div>
+        </div>
+);
+const Main=()=>(
+        <div className="container">
+            <div className="card">
+                <div className="card-img">
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kt09zylanc2qd5eg116a" alt="" />
+                </div>
+                <div className="card-content">
+                    <h3>Apna Sweets</h3>
+                    <h6>Sweet, Beverage,Drink ...</h6>
+                    <h6>Vijay Nagar</h6>
+                </div>
+            </div>
+        </div>
+);
+const Footer=()=>(
+        <h1>Footer</h1>
 );
 
+const AppLayout=()=>(
+    <React.Fragment>
+        <Header />
+        <Main />
+        <Footer />
+    </React.Fragment>
+);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<ContentName></ContentName>);
+root.render(<AppLayout />);
 
